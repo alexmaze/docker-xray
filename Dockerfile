0dfce6e86d6691edc3403e6fcde1cc030b73a298
alexayan <alexayan@tencent.com>
2023-01-18 15:10:46 +0800

    fix script

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM debian:bullseye-slim
 
 RUN mkdir -p /usr/local/share/xray/
 
-COPY ./tmp/xray /usr/bin/xray
-COPY ./tmp/geoip.dat /usr/local/share/xray/geoip.dat
-COPY ./tmp/geosite.dat /usr/local/share/xray/geosite.dat
+ARG TMPPATH=./tmp
+
+COPY ${TMPPATH}/xray /usr/bin/xray
+COPY ${TMPPATH}/geoip.dat /usr/local/share/xray/geoip.dat
+COPY ${TMPPATH}/geosite.dat /usr/local/share/xray/geosite.dat
 
 CMD /usr/bin/xray run -config /etc/xray/config.json
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 XRAY_VERSION=1.7.2
 # XRAY_PLATFORM: https://github.com/xtls/Xray-core/releases
 # e.g. linux-64 / linux-arm64-v8a
-XRAY_PLATFORM=linux-arm64-v8a
+XRAY_PLATFORM=linux-64
 
 filename=Xray-${XRAY_PLATFORM}.zip
 tagname=${XRAY_VERSION}-${XRAY_PLATFORM}
@@ -11,11 +11,12 @@ tmpdir=${workdir}/tmp
 mkdir $tmpdir && cd $tmpdir
 
 wget https://github.com/XTLS/Xray-core/releases/download/v${XRAY_VERSION}/${filename}
-unzip ${filename}
-rm ${filename}
+unzip $filename
+rm $filename
 
 cd $workdir
-rm -rf $tmpdir
 
-docker build --tag alexmaze/xray:${tagname} $workdir
-docker push alexmaze/xray:${tagname}
+docker build --tag alexmaze/xray:$tagname $workdir
+docker push alexmaze/xray:$tagname
+
+rm -rf $tmpdir
